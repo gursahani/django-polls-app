6832cadfcd68b889e65b970c7835b2537e91f7b0
Kunal Gursahani <kunalg.dev@gmail.com>
2020-01-07 02:14:51 -0800

    added 404 get shortcut

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,8 @@
-from django.http import HttpResponse
-from django.template import loader
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 
-# Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {
@@ -14,7 +12,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
